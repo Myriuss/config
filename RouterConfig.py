38a118f576afdaf60130438d6f97ccf9c6fd5e4c
Myriuss <marielelyamani@gmail.com>
2023-08-27 21:19:26 +0100

    :memo: Init

---
+++ RouterConfig.py
@@ -0,0 +1,77 @@
+import telnetlib
+import time
+
+HOST = "192.168.208.1"  # Adresse IP du commutateur
+USER = "sw0"  
+PASSWORD = "password"  
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(USER.encode('ascii') + b"\n")
+
+tn.read_until(b"Password: ")
+tn.write(PASSWORD.encode('ascii') + b"\n")
+
+tn.read_until(b">")  # Ou d'autres prompts selon votre commutateur
+
+# Entrez en mode de configuration
+tn.write(b"enable\n")
+tn.read_until(b"Password: ")
+tn.write(PASSWORD.encode('ascii') + b"\n")
+tn.read_until(b"#")
+
+# Commandes de configuration
+commands = [
+    b"conf t",
+    b"interface vlan1",
+    b"ip address  192.168.10.1 255.255.255.0",
+    b"no shutdown",
+    b"exit",
+    b"vlan 10",
+    b"name vlan10",
+    b"exit",
+    b"int vlan 10",
+    b"ip address  192.168.10.1 255.255.255.0",
+    b"no shut",
+    b"vlan 2",
+    b"name vlan2",
+    b"exit",
+    b"int vlan 2",
+    b"ip address  192.168.2.1 255.255.255.0",
+    b"no shut",
+    b"exit",
+    b"vlan 3",
+    b"name vlan 3",
+    b"exit",
+    b"int vlan 3",
+    b"ip address  192.168.3.1 255.255.255.0",
+    b"no shut",
+    b"exit",
+    b"int range fa0/1-2",
+    b"switchport mode access",
+    b"switchport access vlan 10",
+    b"exit",
+    b"int range fa0/3-5",
+    b"switchport mode access",
+    b"switchport access vlan 2",
+    b"exit",
+    b"int range fa0/6-8",
+    b"switchport mode access",
+    b"switchport access vlan 3",
+    b"exit",
+    b"int fa0/10",
+    b"switchport mode trunk",
+    b"description LINK-TO-ROUTER",
+    b"exit",
+    
+]
+
+for cmd in commands:
+    tn.write(cmd + b"\n")
+    time.sleep(1)  # Attendre un peu entre chaque commande
+
+output = tn.read_all().decode('ascii')
+print(output)
+
+tn.close()
---
+++ sw2Config.py
@@ -0,0 +1,46 @@
+import getpass
+import telnetlib
+
+HOST = "192.168.2.22"
+user = input("Enter your username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Usernmae: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"ls\n")
+tn.write(b"password\n")
+tn.write(b"conf t\n")
+tn.write(b"vlan 4\n")
+tn.write(b"name vlan4\n")
+tn.write(b"no shut\n")
+tn.write(b"exit\n")
+tn.write(b"interface range Ethernet 0/1-2\n")
+tn.write(b"switchport mode access\n")
+tn.write(b"switchport access vlan 4\n")
+tn.write(b"vlan 5\n")
+tn.write(b"name vlan5\n")
+tn.write(b"no shut\n")
+tn.write(b"exit\n")
+tn.write(b"interface Ethernet 0/3\n")
+tn.write(b"switchport mode access\n")
+tn.write(b"switchport access vlan 4\n")
+tn.write(b"exit\n")
+tn.write(b"interface Ethernet 1/0\n")
+tn.write(b"switchport mode access\n")
+tn.write(b"switchport access vlan 4\n")
+tn.write(b"exit\n")
+tn.write(b"interface Ethernet 0/0\n")
+tn.write(b"description LINK-TO-ROUTER5\n")
+tn.write(b"switchport mode trunk\n")
+tn.write(b"exit\n")
+tn.write(b"end\n")
+tn.write(b"exit\n")
+
+
+print(tn.read_all().decode('ascii'))
